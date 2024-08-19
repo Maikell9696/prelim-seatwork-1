39d235d76ad63aad6632f53dc09e84f8779adb33
Lyra Marielle Rodriguez <95603-22256598@SERVER2.DOMAIN>
2024-08-19 18:26:57 +0800

    First Commit

---
+++ Index.js
@@ -0,0 +1,16 @@
+const user =  {
+    fname: "Lyra",
+    lname: "Rodriguez",
+    gender: "F",
+    age: "20",
+}
+let val = ""
+let val1 = ""
+let val2 = ""
+//dot notation
+
+val = user.fname
+val1 = user.lname
+val2 = user.age
+console.log("I am " +   val +  " "  + val1 + " and I am " + val2 +"" + " yrs old")
+
